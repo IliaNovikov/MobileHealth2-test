d8f749613b77d9c88fb15dea7fa98d3b7a893536
CUMrade228 <ilia.pp.novikov@gmail.com>
2023-05-20 18:32:13 +0500

    заготовки фрагмента тренировок

---
+++ app/src/main/java/com/example/mobilehealth/WorkoutExercisesFragment.java
@@ -0,0 +1,23 @@
+package com.example.mobilehealth;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class WorkoutExercisesFragment extends Fragment {
+
+    public WorkoutExercisesFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_workout_exercises, container, false);
+    }
+}
